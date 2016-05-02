e2c82b3f403785abff1ea8ed28cbe7d896637e0d
Tina Cormier <tcormier@whrc.org>
2016-05-02 14:34:59 -0400

    updated mapID and accessToken

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,10 +2,10 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'will-breitkreutz.k6fj4l3f';
+var mapId = 'tinacormier.019h3jej';
 
 // And this is my access token, use yours.
-var accessToken = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw';
+var accessToken = 'pk.eyJ1IjoidGluYWNvcm1pZXIiLCJhIjoiY2lucHozdW03MTAzNXR0bTMwcmZ6eWZzayJ9.y4GzGC6WEC3JBskj9s2v0A';
 
 // Create the map object with your mapId and token,
 // referencing the DOM element where you want the map to go.
