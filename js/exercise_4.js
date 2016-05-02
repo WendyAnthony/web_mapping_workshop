dec7cc728f21e164f347fe38684d531b2968f82c
Tina Cormier <tcormier@whrc.org>
2016-05-02 18:07:51 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -167,18 +167,18 @@ function getDirections(frm,to) {
         })
         
         // This is where I got lost! Should give some directions.
-      	$('#directions').fadeIn(400, function(){
-      		$('#summary').empty();
-      		$('#distance').text((Math.round(data.trip.summary.length * 100) / 100) + data.trip.units);
-      		$('#time').text((Math.round(data.trip.summary.time / 60 * 100) / 100) + ' min');
-        })
+            $('#directions').fadeIn(400, function(){
+      $('#summary').empty();
+      $('#distance').text((Math.round(data.trip.summary.length * 100) / 100) + data.trip.units);
+      $('#time').text((Math.round(data.trip.summary.time / 60 * 100) / 100) + ' min');
 
-      	data.trip.legs[0].maneuvers.forEach(function(item){
-       		var direction = '';
-        	direction += '<li class="instruction" data-begin=' + item.begin_shape_index + ' data-end=' + item.end_shape_index + '>';
-        	if(item.verbal_post_transition_instruction) direction += '<p class="post-transition">' + item.verbal_post_transition_instruction + '</p>';
-        	if(item.verbal_pre_transition_instruction) direction += '<p class="pre-transition">' + item.verbal_pre_transition_instruction + '</p>';
-        	direction += '</li>';
-        	$('#summary').append(direction);
+      data.trip.legs[0].maneuvers.forEach(function(item){
+        var direction = '';
+        direction += '<li class="instruction" data-begin=' + item.begin_shape_index + ' data-end=' + item.end_shape_index + '>';
+        if(item.verbal_post_transition_instruction) direction += '<p class="post-transition">' + item.verbal_post_transition_instruction + '</p>';
+        if(item.verbal_pre_transition_instruction) direction += '<p class="pre-transition">' + item.verbal_pre_transition_instruction + '</p>';
+        direction += '</li>';
+        $('#summary').append(direction);
       })
+    })
    
