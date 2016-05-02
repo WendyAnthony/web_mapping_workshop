a869aa4c29b1a36fbd3f4cf11ac951301183546c
Tina Cormier <tcormier@whrc.org>
2016-05-02 15:55:10 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -79,11 +79,11 @@ var clickHandler = function(e){
   
 }
 
-featureLayer.on('ready'), function() {
+featureLayer.on('ready', function() {
 	this.eachLayer(function(layer) {
     	layer.on('click', clickHandler)
     })
-}
+})
 
 map.on('click', function(){
 	$('#sidebar').fadeOut(200);	
