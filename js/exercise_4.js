1a8be9f62b5448a69cad430a370e02d76b9f54f4
Tina Cormier <tcormier@whrc.org>
2016-05-03 14:41:42 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -130,55 +130,38 @@ var routeLine = L.mapbox.featureLayer().addTo(map);
 
 // Need function to take from point and to point and call their service
 function getDirections(frm,to) {
-  	var jsonPayload = JSON.stringify({
-    	locations: [
-          {lat: frm[1], lon: frm[0]},
-          {lat: to[1], lon: to[0]}
-        ],
-      	// tell it how to prioritize routes
-      	costing: 'pedestrian',
-      	directions_options: {
-      		units: 'miles'
+  var jsonPayload = JSON.stringify({
+    locations: [
+      {lat: frm[1], lon: frm[0]},
+      {lat: to[1], lon: to[0]}
+    ],
+        // tell it how to prioritize routes
+        costing: 'pedestrian',
+        directions_options: {
+          units: 'miles'
         }
     })
-    
-    // now need a way to call the service. Since we are using jquery (access it using $).
-    $.ajax({
-    	url: 'https://valhalla.mapzen.com/route',
-      	data: {
-          	json: jsonPayload,
-          	// Need to get your own key after this workshop. Go to the website and authenticate w/ github.
-          	api_key: 'valhalla-gwtf3x2'
-        }
-    }).done(function(data){
-    	var routeShape = polyline.decode(data.trip.legs[0].shape);
-        routeLine.setGeoJSON({
-          	type: 'Feature',
-          	geometry: {
-            	type: 'LineString',
-              	coordinates: routeShape
-            },
-          	properties: {
-              	// Mapbox simple style spec.
-            	"stroke": "#ed23f1",
-              	"stroke-opacity": 0.8,
-             	"stroke-width": 8
-            }
-        })
-        
-        // This is where I got lost! Should give some directions.
-            $('#directions').fadeIn(400, function(){
-      $('#summary').empty();
-      $('#distance').text((Math.round(data.trip.summary.length * 100) / 100) + data.trip.units);
-      $('#time').text((Math.round(data.trip.summary.time / 60 * 100) / 100) + ' min');
 
-      data.trip.legs[0].maneuvers.forEach(function(item){
-        var direction = '';
-        direction += '<li class="instruction" data-begin=' + item.begin_shape_index + ' data-end=' + item.end_shape_index + '>';
-        if(item.verbal_post_transition_instruction) direction += '<p class="post-transition">' + item.verbal_post_transition_instruction + '</p>';
-        if(item.verbal_pre_transition_instruction) direction += '<p class="pre-transition">' + item.verbal_pre_transition_instruction + '</p>';
-        direction += '</li>';
-        $('#summary').append(direction);
-      })
+  // now need a way to call the service. Since we are using jquery (access it using $).
+  $.ajax({
+    url:'http://valhalla.mapzen.com/route',
+    data:{
+      json: jsonPayload,
+      api_key: 'valhalla-gwtf3x2'
+    }
+  }).done(function(data){
+    var routeShape = polyline.decode(data.trip.legs[0].shape);
+    routeLine.setGeoJSON({
+      type:'Feature',
+      geometry:{
+        type:'LineString',
+        coordinates: routeShape
+      },
+      properties:{
+        "stroke": '#ed23f1',
+        "stroke-opacity": 0.8,
+        "stroke-width": 8
+      }
     })
-   
+  })
+} // End getDirections
